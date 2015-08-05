efaca28396828c5fda9abe411334de4a8d748153
John <john.downs@acm.org>
2015-08-05 19:56:07 -0400

    initializing projects

---
+++ Load-Test-Service/test/load_test_service/core_test.clj
@@ -0,0 +1,7 @@
+(ns load-test-service.core-test
+  (:require [clojure.test :refer :all]
+            [load-test-service.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
