47a3141230a2aec95ab54b7f9002c99e5bafc72f
Michael Willis <willismichael@gmail.com>
2015-08-06 18:49:07 -0500

    Untangled defmacro load-test

--- Runner/src/runner/context.clj
+++ Runner/src/runner/context.clj
@@ -6,20 +6,20 @@
 (def ^:dynamic *thread-number*)
 (def ^:dynamic *run-number*)
 
+(defn- run-load-test
+  [f {:keys [threads runs] :or {threads 10 runs 10}}]
+  (let [results (doall
+                 (for [t (range threads)]
+                   (thread
+                     (doall
+                      (for [r (range runs)]
+                        (binding [*thread-number* t
+                                  *run-number* r]
+                          (f)))))))]
+    (go (doseq [result results] (<!! result)))))
+
 (defmacro load-test [params & body]
-  (let [defaults {:threads 10 :runs 10 :delay 0}
-        params (merge defaults params)
-        run (fn [{:keys [threads runs delay]} f]
-              (let [results (doall
-                             (for [t (range threads)]
-                               (thread
-                                 (doall
-                                  (for [r (range runs)]
-                                    (binding [*thread-number* t
-                                              *run-number* r]
-                                      (f)))))))]
-                (go (doseq [result results] (<!! result)))))]
-    `(~run ~params (fn [] ~@body))))
+  `(~run-load-test (fn [] ~@body) ~params))
 
 (letfn [(wrap [f]
           (fn [& args]
