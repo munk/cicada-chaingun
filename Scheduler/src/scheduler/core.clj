2e05b69f36504ceee3bcc392c602437b78158c61
Jai Pidugu <jai.pidugu@yodle.com>
2015-08-06 11:31:29 -0400

    its working!!!

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -1,7 +1,15 @@
 (ns scheduler.core
+  (:require [clj-mesos.scheduler]
+            [clojure.pprint])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def myscheduler
+  (clj-mesos.scheduler/scheduler (registered [driver fid mi]
+                                             (println "registered" fid mi))
+                                 (resourceOffers [driver offers]
+                                                 (clojure.pprint/pprint offers))))
