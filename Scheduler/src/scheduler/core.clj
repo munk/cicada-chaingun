9504e11ae4135b6a0add07823abe4c42fa8988d9
Jai Pidugu <jai.pidugu@yodle.com>
2015-08-06 16:38:54 -0400

    something that doesn't work

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -1,20 +1,32 @@
 (ns scheduler.core
-  (:require [clj-mesos.scheduler]
-            [clojure.pprint])
+  (:require [clojure.core.async :refer [put! close! reduce chan]]
+            [mesomatic.scheduler :as sched]
+            [mesomatic.async.scheduler :as async]
+            )
   (:gen-class))
 
-(def myscheduler
-  (clj-mesos.scheduler/scheduler (registered [driver fid mi]
-                                             (println "registered" fid mi))
-                                 (resourceOffers [driver offers]
-                                                 (clojure.pprint/pprint offers))))
+(defmulti handle-mesos-message (fn [_ message] (:type message)))
 
-(def overdriver
-  (clj-mesos.scheduler/driver
-    myscheduler {:user "" :name "testframework"} "dev-mesos-master5.nyc.dev.yodle.com:5050"))
+(defmethod handle-mesos-message :default
+  [state {:keys [offers]}]
+  (println "hello")
+  (assoc state :offers offers))
+
+(defmethod handle-mesos-message :registered
+  [& stuff]
+  (println stuff))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (clj-mesos.scheduler/start overdriver)
-  (clj-mesos.scheduler/stop overdriver))
+
+  (let [sched (sched/scheduler )
+        framework {:name "cicada-chaingun"}
+        driver (sched/scheduler-driver sched framework "dev-mesos-master5.nyc.dev.yodle.com:5050")]
+    (println "hello2")
+    (sched/start! driver)
+    (println "hello3")
+    (while true
+      (reduce handle-mesos-message {:driver driver} ch)
+      (Thread/sleep 5000))
+    ))
