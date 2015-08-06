12572db2e0014d8aeb564e004aad6ea5ae869c7d
Jeffrey Simon <jsimon@yodle.com>
2015-08-06 13:06:41 -0400

    initial boot of load test service

--- Load-Test-Service/project.clj
+++ Load-Test-Service/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-ring "0.9.6"]]
+  :ring { :handler load-test-service.core/handler}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [liberator "0.13"]
+                 [compojure "1.4.0"]
+                 [ring/ring-core "1.4.0"]]
   :main ^:skip-aot load-test-service.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- Load-Test-Service/src/load_test_service/core.clj
+++ Load-Test-Service/src/load_test_service/core.clj
@@ -1,7 +1,31 @@
 (ns load-test-service.core
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn create-load-test-result []
+  (let [id (str (java.util.UUID/randomUUID))]
+    {::id id }))
+
+(defn get-load-test-result [id]
+  {:id id})
+
+(defresource load-test-result-entry []
+             :available-media-types ["text/plain"]
+             :allowed-methods [:post]
+             :post! (fn [ctx] (create-load-test-result))
+             :post-redirect? (fn [ctx] {:location (format "/result/%s" (::id ctx))}))
+
+(defresource load-test-result-entry-resource [id]
+             :available-media-types ["application/edn"]
+             :allowed-methods [:get]
+             :handle-ok (fn [ctx] (get-load-test-result id)))
+
+(defroutes app
+  (ANY "/result" [] (load-test-result-entry))
+  (ANY "/result/:id" [id] (load-test-result-entry-resource id)))
+
+(def handler
+  (-> app
+      wrap-params))
