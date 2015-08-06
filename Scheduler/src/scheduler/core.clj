d06073df3438348ccf90e0a384dbdb4b6259bb59
john.downs <john.downs@yodle.com>
2015-08-06 17:31:24 -0400

    it works!! again...

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -1,32 +1,37 @@
 (ns scheduler.core
   (:require [clojure.core.async :refer [put! close! reduce chan]]
             [mesomatic.scheduler :as sched]
-            [mesomatic.async.scheduler :as async]
             )
   (:gen-class))
 
-(defmulti handle-mesos-message (fn [_ message] (:type message)))
-
-(defmethod handle-mesos-message :default
-  [state {:keys [offers]}]
-  (println "hello")
-  (assoc state :offers offers))
-
-(defmethod handle-mesos-message :registered
-  [& stuff]
-  (println stuff))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (let [sched (sched/scheduler )
+  (let [sched (sched/scheduler
+                (registered        [this driver framework-id master-info]
+                                   (println "Registered"))
+                (reregistered      [this driver master-info]
+                                   (println "reregistered"))
+                (disconnected      [this driver]
+                                   (println "disconnected"))
+                (resource-offers   [this driver offers]
+                                   (println "resource offers"))
+                (offer-rescinded   [this driver offer-id]
+                                   (println "offer recinded"))
+                (status-update     [this driver status]
+                                   (println "status update"))
+                (framework-message [this driver executor-id slave-id data]
+                                   (println "framework msg"))
+                (slave-lost        [this driver slave-id]
+                                   (println "slave lost"))
+                (executor-lost     [this driver executor-id slave-id status]
+                                   (println "executor lost"))
+                (error             [this driver message]
+                                   (println "error")))
         framework {:name "cicada-chaingun"}
-        driver (sched/scheduler-driver sched framework "dev-mesos-master5.nyc.dev.yodle.com:5050")]
-    (println "hello2")
+        driver (sched/scheduler-driver sched framework "10.100.17.164:5050")]
     (sched/start! driver)
-    (println "hello3")
     (while true
-      (reduce handle-mesos-message {:driver driver} ch)
-      (Thread/sleep 5000))
-    ))
+      (Thread/sleep 1000))))
