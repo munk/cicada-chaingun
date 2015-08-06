0c4f1b33b682214f2c20cd248790a072decd8438
Jai Pidugu <jai.pidugu@yodle.com>
2015-08-06 11:47:56 -0400

    made a mesos scheduler

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -3,13 +3,18 @@
             [clojure.pprint])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (def myscheduler
   (clj-mesos.scheduler/scheduler (registered [driver fid mi]
                                              (println "registered" fid mi))
                                  (resourceOffers [driver offers]
                                                  (clojure.pprint/pprint offers))))
+
+(def overdriver
+  (clj-mesos.scheduler/driver
+    myscheduler {:user "" :name "testframework"} "dev-mesos-master5.nyc.dev.yodle.com:5050"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (clj-mesos.scheduler/start overdriver)
+  (clj-mesos.scheduler/stop overdriver))
