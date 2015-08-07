de44cb32e6d89eab86692fbc38b1a41b6eb0484e
Jai Pidugu <jai.pidugu@yodle.com>
2015-08-07 12:21:20 -0400

    declining offers works now

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -11,24 +11,27 @@
 
 (defmethod handle-message :registered
   [driver _]
-  (println "Registered:" (:driver driver)))
+  (println "Registered:" (:driver driver))
+   driver)
 
 (defmethod handle-message :resource-offers
-  [state {:keys [offers]}]
+  [{:keys [driver] :as state} {:keys [offers]}]
   (loop []
     (let [[task _] (alts!! [task-channel (timeout 50)])]
       (if task
         (do
           (println "submitting task:" task)
           (recur))
-        (println "no tasks"))))
+        (do
+          (println "no tasks")
+          (sched/decline-offer driver (-> offers first :id))))))
   (println "Offers: " offers)
-  (let [task-id 1
-        container-info []]))
+   state)
 
 (defmethod handle-message :default
   [state message]
-  (println "wtf:" message))
+  (println "wtf:" message)
+   state)
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -37,7 +40,7 @@
   (let [ch (chan)
         sched (masync/scheduler ch)
         framework {:name "cicada-chaingun"}
-        driver (sched/scheduler-driver sched framework "10.100.17.164:5050")]
+        driver (sched/scheduler-driver sched framework "10.100.25.110:5050")]
     (sched/start! driver)
     (async/reduce handle-message {:driver driver} ch)
     (while true
