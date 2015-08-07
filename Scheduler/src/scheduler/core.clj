b3ba1914b2068e07e1fcabfb2feb13e97a96681d
john.downs <john.downs@yodle.com>
2015-08-07 10:47:14 -0400

    some additions to core to run the container. is broken

--- Scheduler/src/scheduler/core.clj
+++ Scheduler/src/scheduler/core.clj
@@ -1,37 +1,38 @@
 (ns scheduler.core
   (:require [clojure.core.async :refer [put! close! reduce chan]]
             [mesomatic.scheduler :as sched]
+            [mesomatic.types :as mtypes]
+            [mesomatic.async.scheduler :as async]
             )
   (:gen-class))
 
+(defmulti handle-message (fn [_ message] (:type message)))
+
+(defmethod handle-message :registered
+  [driver _]
+  (println "Registered:" (:driver driver)))
+
+(defmethod handle-message :resource-offers
+  [state {:keys [offers]}]
+  (println "Offers: " offers)
+  (let [task-id (mtypes/TaskId. 1)
+        container-info (mtypes/)
+        ])
+  )
+
+(defmethod handle-message :default
+  [state message]
+  (println "wtf:" message))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
 
-  (let [sched (sched/scheduler
-                (registered        [this driver framework-id master-info]
-                                   (println "Registered"))
-                (reregistered      [this driver master-info]
-                                   (println "reregistered"))
-                (disconnected      [this driver]
-                                   (println "disconnected"))
-                (resource-offers   [this driver offers]
-                                   (println "resource offers"))
-                (offer-rescinded   [this driver offer-id]
-                                   (println "offer recinded"))
-                (status-update     [this driver status]
-                                   (println "status update"))
-                (framework-message [this driver executor-id slave-id data]
-                                   (println "framework msg"))
-                (slave-lost        [this driver slave-id]
-                                   (println "slave lost"))
-                (executor-lost     [this driver executor-id slave-id status]
-                                   (println "executor lost"))
-                (error             [this driver message]
-                                   (println "error")))
+  (let [ch (chan)
+        sched (async/scheduler ch)
         framework {:name "cicada-chaingun"}
         driver (sched/scheduler-driver sched framework "10.100.17.164:5050")]
     (sched/start! driver)
+    (reduce handle-message {:driver driver} ch)
     (while true
       (Thread/sleep 1000))))
